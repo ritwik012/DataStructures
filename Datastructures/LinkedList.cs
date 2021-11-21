63a5f1cb077735915b206c72396142ce450168d8
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-21 20:07:05 +0530

    [Rutwik]Added:SimpleLinkList File

---
+++ Datastructures/LinkedList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    public class LinkedList
+    {
+        internal Node head;
+        internal void Add(int data)
+        {
+            Node node = new Node(data);
+            if (this.head == null)
+            {
+                this.head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine("{0} is inserted into the linked list", node.data);
+        }
+    }
+}
---
+++ Datastructures/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace DataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList linkedList = new LinkedList();
+            Console.WriteLine("Welcome to Data Structures Programs");
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        break;
+                    case 2:
+                        flag = false;
+                        break;
+                }
+            }
+
+        }
+    }
+}
