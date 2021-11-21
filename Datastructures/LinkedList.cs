06acd1da726e7370a9a0efa9b253d2320002ea5a
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-21 20:12:41 +0530

    [Rutwik]Added:AddInReverseOrder File

--- Datastructures/LinkedList.cs
+++ Datastructures/LinkedList.cs
@@ -25,5 +25,34 @@ namespace DataStructures
             }
             Console.WriteLine("{0} is inserted into the linked list", node.data);
         }
+        internal void AddInReverseOrder(int data)
+        {
+            Node newNode = new Node(data);
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node temp = this.head;
+                head = newNode;
+                head.next = temp;
+            }
+        }
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("LinkedList is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine();
+        }
     }
 }
--- Datastructures/Program.cs
+++ Datastructures/Program.cs
@@ -11,7 +11,7 @@ namespace DataStructures
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Exit");
+                Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Add in Reverse Order \n 3. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -21,6 +21,12 @@ namespace DataStructures
                         linkedList.Add(70);
                         break;
                     case 2:
+                        linkedList.AddInReverseOrder(70);
+                        linkedList.AddInReverseOrder(30);
+                        linkedList.AddInReverseOrder(56);
+                        linkedList.Display();
+                        break;
+                    case 3:
                         flag = false;
                         break;
                 }
