c1a4a0ff0dc3babe35a9a4235ddd806ade21f781
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-11-22 09:56:44 +0530

    [Rutwik]Added:Search File

--- Datastructures/LinkedList.cs
+++ Datastructures/LinkedList.cs
@@ -107,5 +107,21 @@ namespace DataStructures
             NewNode.next = null;
             return head;
         }
+        internal int Search(int value)
+        {
+            Node node = this.head;
+            int count = 0;
+            while (node != null)
+            {
+
+                if (node.data == value)
+                {
+                    return count;
+                }
+                node = node.next;
+                count++;
+            }
+            return count;
+        }
     }
 }
--- Datastructures/Program.cs
+++ Datastructures/Program.cs
@@ -7,11 +7,13 @@ namespace DataStructures
         static void Main(string[] args)
         {
             LinkedList linkedList = new LinkedList();
+            Stack stack = new Stack();
+            Queue queue = new Queue();
             Console.WriteLine("Welcome to Data Structures Programs");
             bool flag = true;
             while (flag)
             {
-                Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Add in Reverse Order \n 3. Insert at Particular Position \n 4. Remove First node \n 5. Remove Last Node \n 6. Exit");
+                Console.WriteLine("Enter the Program to be executed : \n 1. Create Simple Linked List \n 2. Add in Reverse Order \n 3. Insert at Particular Position \n 4. Remove First node \n 5. Remove Last Node \n 6. Stack Push \n 7. Stack Peek and Pop \n 8. Enqueue \n 9. Dequeue \n 10. Search \n 11. Ability to Insert \n 12. Exit");
                 int option = Convert.ToInt32(Console.ReadLine());
                 switch (option)
                 {
@@ -49,6 +51,63 @@ namespace DataStructures
                         linkedList.Display();
                         break;
                     case 6:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Display();
+                        break;
+                    case 7:
+                        stack.Push(70);
+                        stack.Push(30);
+                        stack.Push(56);
+                        stack.Display();
+                        stack.Peek();
+                        stack.Pop();
+                        stack.Display();
+                        stack.Peek();
+                        stack.Pop();
+                        stack.Display();
+                        stack.Peek();
+                        stack.Pop();
+                        stack.Display();
+                        break;
+                    case 8:
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        queue.Display();
+                        break;
+                    case 9:
+                        queue.Enqueue(56);
+                        queue.Enqueue(30);
+                        queue.Enqueue(70);
+                        queue.Display();
+                        queue.Dequeue();
+                        queue.Display();
+                        queue.Dequeue();
+                        queue.Display();
+                        queue.Dequeue();
+                        queue.Display();
+                        break;
+                    case 10:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.Display();
+                        int search = linkedList.Search(30);
+                        Console.WriteLine("The searched data is at position {0}", search);
+                        break;
+                    case 11:
+                        linkedList.Add(56);
+                        linkedList.Add(30);
+                        linkedList.Add(70);
+                        linkedList.Display();
+                        int position = linkedList.Search(30);
+                        position++;
+                        linkedList.InsertAtParticularPosition(position, 40);
+                        linkedList.Display();
+                        break;
+                    case 12:
                         flag = false;
                         break;
                 }
